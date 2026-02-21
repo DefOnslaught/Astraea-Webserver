0a4eaed596dd1036565fa7c304f5c8eeebfbd2d4
Darren Bilodeau <darrenbilodeau@gmail.com>
2026-02-21 02:31:56 +0000

    Implemented Redis

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -87,6 +87,20 @@ TEMPLATES = [
 WSGI_APPLICATION = 'backend.wsgi.application'
 
 
+# Caching related info
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": os.getenv("REDIS_URL", "redis://127.0.0.1:6379/1"),
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
+CACHE_MIDDLEWARE_KEY_PREFIX = "astraea"
+
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
