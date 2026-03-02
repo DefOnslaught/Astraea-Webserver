603d8803cce228c2406589b96f4067fa40847899
Darren Bilodeau <darrenbilodeau@gmail.com>
2026-03-02 05:55:09 +0000

    Added users to admin page

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    fields = ('id', 'last_login', 'is_superuser', 'first_name', 'last_name', 'is_staff', 'email', 'username')
+    list_display = ('username',  'email', 'id', 'last_login', 'is_superuser', 'first_name', 'last_name', 'is_staff')
