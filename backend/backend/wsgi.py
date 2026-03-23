5b9ca6df67b5c7cb98cbc65111eb2269a44ee9e4
Darren Bilodeau <darrenbilodeau@gmail.com>
2026-03-23 17:56:11 -0400

    Improved how gevent is loaded

--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -1,13 +1,7 @@
-"""
-WSGI config for backend project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
 import os
+import gevent.monkey
+
+gevent.monkey.patch_all()
 
 from django.core.wsgi import get_wsgi_application
 
