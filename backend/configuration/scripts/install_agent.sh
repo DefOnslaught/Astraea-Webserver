db0018a766c247e6891d14cab1e59691bf514119
Darren Bilodeau <darrenbilodeau@gmail.com>
2026-03-31 21:49:57 -0400

    Properly creates the env file

--- backend/configuration/scripts/install_agent.sh
+++ backend/configuration/scripts/install_agent.sh
@@ -7,6 +7,7 @@ API_KEY="{{ API_KEY }}"
 BASE_URL="{{ BASE_URL }}"
 UID="{{ UID }}"
 EXE_LOGIC="{{ EXE_LOGIC }}"
+ENVIRONMENT="{{ ENVIRONMENT }}"
 CRON_SCHEDULE="{{ CRON }}"
 
 INSTALL_DIR="/opt/Astraea-Agent"
@@ -14,7 +15,7 @@ INSTALL_DIR="/opt/Astraea-Agent"
 echo "--- Fetching Astraea Agent Package ---"
 mkdir -p $INSTALL_DIR
 # Use internal API key to download the core tarball
-curl -sSL -H "X-Internal-Api-Key: $API_KEY" -o astraea_agent.tar.gz "$BASE_URL/api/config/agent_file/"
+curl -sSL -H "X-Api-Key: $API_KEY" -o astraea_agent.tar.gz "$BASE_URL/api/config/agent_file/"
 tar -xzf astraea_agent.tar.gz -C $INSTALL_DIR
 cd $INSTALL_DIR
 
@@ -28,14 +29,26 @@ if [ "$EXE_LOGIC" != "standard" ]; then
     chmod +x "patching-$EXE_LOGIC.sh"
 fi
 
-# 3. Edit .env safely
+# 3. Configures '.env'
+if [ -f ".env_example" ]; then
+    if [ ! -f ".env" ]; then
+        cp ".env_example" "backend/.env"
+        echo ".env"
+    else
+        echo ".env already exists, skipping copy."
+    fi
+else
+    echo "Warning: env_example not found."
+fi
+
+# 4. Edit .env
 sed -i "s|^API_KEY=.*|API_KEY=$API_KEY|" .env
-sed -i "s|^ENV=.*|ENV={{ ENVIRONMENT }}|" .env
+sed -i "s|^ENV=.*|ENV=$ENVIRONMENT|" .env
 sed -i "s|^BASE_URL=.*|BASE_URL=$BASE_URL|" .env
 
 echo "--- Configuration Applied ---"
 
-# 4. Determine Execution Path
+# 5. Determine Execution Path
 if [ "$EXE_LOGIC" == "week1and3" ]; then
     EXEC_CMD="/bin/bash $INSTALL_DIR/patching-week1and3.sh"
 elif [ "$EXE_LOGIC" == "week2and4" ]; then
@@ -44,7 +57,7 @@ else
     EXEC_CMD="/usr/bin/python3 $INSTALL_DIR/core/initialize.py"
 fi
 
-# 5. Cron Setup
+# 6. Cron Setup
 (crontab -l 2>/dev/null | grep -v "$INSTALL_DIR"; echo "$CRON_SCHEDULE cd $INSTALL_DIR && $EXEC_CMD >> /var/log/astraea-patch-cron.log 2>&1") | crontab -
 
 echo "--- Installation Complete: Logic [$EXE_LOGIC] applied ---"
