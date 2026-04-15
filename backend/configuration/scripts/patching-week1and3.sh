772e7c1e1473345bf732c961fa8bcd8fa032c6d8
Darren Bilodeau <darrenbilodeau@gmail.com>
2026-04-14 23:14:52 -0400

    added '-' to the date command to remove the leading 0

--- backend/configuration/scripts/patching-week1and3.sh
+++ backend/configuration/scripts/patching-week1and3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Logic: Run only if the day is 1-7 (Week 1) or 15-21 (Week 3)
-DOM=$(date +%d)
+DOM=$(date +%-d)
 
 if (( DOM >= 1 && DOM <= 7 )) || (( DOM >= 15 && DOM <= 21 )); then
     /usr/bin/python3 /opt/Astraea-Agent/core/initialize.py
--- backend/configuration/scripts/patching-week2and4.sh
+++ backend/configuration/scripts/patching-week2and4.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Logic: Run only if the day is 8-14 (Week 2) or 22-31 (Week 4+)
-DOM=$(date +%d)
+DOM=$(date +%-d)
 
 if (( DOM >= 8 && DOM <= 14 )) || (( DOM >= 22 && DOM <= 31 )); then
     /usr/bin/python3 /opt/Astraea-Agent/core/initialize.py
