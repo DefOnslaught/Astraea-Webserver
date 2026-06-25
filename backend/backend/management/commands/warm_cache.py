e1101f7f260d06cacfa02da093fc7593a39fa250
Darren Bilodeau <darrenbilodeau@gmail.com>
2026-06-25 18:33:45 -0400

    Separated cache functions outside of command

--- backend/backend/management/commands/warm_cache.py
+++ backend/backend/management/commands/warm_cache.py
@@ -1,9 +1,6 @@
 from django.core.management.base import BaseCommand
 
-from servers.models import Server
-from servers.utils import refresh_dashboard_stats, cache_individual_vms, refresh_package_search_index
-from configuration.utils import cache_active_api_keys, get_sys_config, get_zabbix_config, get_notification_config, get_notification_services
-from users.utils import cacheVerificationStatus
+from backend.utils import cache_functions
 
 # Main use is to be ran before Django boots up, that way everything is in cache for the first user
 
@@ -14,35 +11,6 @@ class Command(BaseCommand):
         
         self.stdout.write("Fetching servers from database...")
 
-        # Evaluate the queryset into a list immediately to hit DB once
-        vms = Server.objects.prefetch_related('interfaces').all()
-        
-        # 1. Cache individual VM details
-        cache_individual_vms(vms)
-        
-        # 2. Cache Dashboard stats using the same list
-        refresh_dashboard_stats(vms=vms)
-
-        # 3. Cache all packages that are latest success session for easy searching
-        refresh_package_search_index()
-
-        # 4. Cache all active API keys so patching script auth goes quickly
-        cache_active_api_keys()
-
-        # 5. Cache the System Configuration (i.e, Global Patching Enable )
-        sys_config = get_sys_config()
-        
-        # 6. Cache all users verification status if 'skip_email_validation' is true
-        if not sys_config.get('skip_email_validation'):
-            cacheVerificationStatus()
-        
-        # 7. Cache the Notification configuration
-        get_notification_config()
-
-        # 8. Cache Notification Services
-        get_notification_services()
-
-        # 8. Cache the Zabbix configuration
-        get_zabbix_config()
+        cache_functions()
         
         self.stdout.write(self.style.SUCCESS(f"Successfully warmed cache."))
---
+++ backend/backend/utils.py
@@ -0,0 +1,42 @@
+from django.core.cache import cache
+
+from servers.models import Server
+from servers.utils import refresh_dashboard_stats, cache_individual_vms, refresh_package_search_index
+from configuration.utils import cache_active_api_keys, get_sys_config, get_zabbix_config, get_notification_config, get_notification_services
+from users.utils import cacheVerificationStatus
+
+def cache_functions(clear_cache=False):
+
+    if clear_cache:
+        cache.clear()
+
+    # Evaluate the queryset into a list immediately to hit DB once
+    vms = Server.objects.prefetch_related('interfaces').all()
+        
+    # 1. Cache individual VM details
+    cache_individual_vms(vms)
+        
+    # 2. Cache Dashboard stats using the same list
+    refresh_dashboard_stats(vms=vms)
+
+    # 3. Cache all packages that are latest success session for easy searching
+    refresh_package_search_index()
+
+    # 4. Cache all active API keys so patching script auth goes quickly
+    cache_active_api_keys()
+
+    # 5. Cache the System Configuration (i.e, Global Patching Enable )
+    sys_config = get_sys_config()
+        
+    # 6. Cache all users verification status if 'skip_email_validation' is true
+    if not sys_config.get('skip_email_validation'):
+        cacheVerificationStatus()
+        
+    # 7. Cache the Notification configuration
+    get_notification_config()
+
+    # 8. Cache Notification Services
+    get_notification_services()
+
+    # 8. Cache the Zabbix configuration
+    get_zabbix_config()
