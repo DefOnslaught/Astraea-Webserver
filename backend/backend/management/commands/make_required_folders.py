c527ae51e70f847d27d0c3781fa9a9f317ea52e1
Darren Bilodeau <darrenbilodeau@gmail.com>
2026-07-14 11:37:55 -0400

    Removed useless output

--- backend/backend/management/commands/make_required_folders.py
+++ backend/backend/management/commands/make_required_folders.py
@@ -13,7 +13,5 @@ class Command(BaseCommand):
             os.makedirs(protected_storage_path, exist_ok=True)
             if not os.access(protected_storage_path, os.W_OK):
                 self.stdout.write(self.style.WARNING(f'Directory {protected_storage_path} exists but is not writeable!'))
-            else:
-                self.stdout.write(self.style.SUCCESS('Protected storage directory verified.'))
         except Exception as e:
             self.stdout.write(self.style.ERROR(f'Failed to create directory: {e}'))
